2101cf02a5cd2f3b9f4b42d0798859b426f4543c
user <user@users-MacBook-Pro.local>
2017-08-25 15:39:53 -0700

    riri2

--- ririso/ViewController.swift
+++ ririso/ViewController.swift
@@ -13,8 +13,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("riri")
+        
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
